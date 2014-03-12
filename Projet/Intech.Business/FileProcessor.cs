4638526c9c47827bf521b85b73055eb730e90896
boselli <boselli@intechinfo.fr>
2014-03-12 11:47:35 +0100

    modif projet business

--- Projet/Intech.Business/FileProcessor.cs
+++ Projet/Intech.Business/FileProcessor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,58 @@ namespace Intech.Business
 {
     public class FileProcessor
     {
-        public Result Process( string path )
+        public FileProcessor()
         {
-            return new Result();
+        }
+
+        public FileProcessorResult Process(String path)
+        {
+            FileProcessorResult result = new FileProcessorResult(path);
+
+            if (Directory.Exists(path))
+                ProcessDirectory(path, result);
+            else
+                throw new Exception("Directory not exists");
+
+            return result;
+        }
+
+        private void ProcessDirectory(string path, FileProcessorResult result, bool parentIsHidden = false)
+        {
+            string[] subDirectories = Directory.GetDirectories(path);
+            foreach (var subDirectory in subDirectories)
+                ProcessSubDirectory(subDirectory, result, parentIsHidden);
+
+            string[] files = Directory.GetFiles(path);
+            foreach (var file in files)
+                ProcessFile(file, result, parentIsHidden);
+        }
+
+        private void ProcessSubDirectory(string name, FileProcessorResult result, bool parentIsHidden)
+        {
+            if ((File.GetAttributes(name) & FileAttributes.Hidden) == FileAttributes.Hidden)
+            {
+                result.HiddenDirectoryCount++;
+                parentIsHidden = true;
+            }
+
+            result.TotalDirectoryCount++;
+            ProcessDirectory(name, result, parentIsHidden);
+        }
+
+        private void ProcessFile(string name, FileProcessorResult result, bool parentIsHidden)
+        {
+            if (parentIsHidden)
+                result.UnaccessibleFileCount++;
+
+            if ((File.GetAttributes(name) & FileAttributes.Hidden) == FileAttributes.Hidden)
+            {
+                result.HiddenFileCount++;
+                if (!parentIsHidden)
+                    result.UnaccessibleFileCount++;
+            }
+
+            result.TotalFileCount++;
         }
     }
 }
