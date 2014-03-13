775da4bc54507696a3f1a6d12e49c2d527a679ad
boselli <boselli@intechinfo.fr>
2014-03-13 11:05:58 +0100

    commit

---
+++ Projet/Intech.Business.Test/PremierTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Intech.Business.Test
+{
+        [TestFixture]
+        public class PremierTest
+        {
+            [Test]
+            public void TestMethod1()
+            {
+                int x1 = 1;
+                int x2 = 2;
+                int sum;
+
+                sum = x1 + x2;
+
+                Assert.That(sum == 3);
+
+            }
+        }
+}
